73095d76a191a1770521475d9c6e3424a436101c
Jolx77 <joaquin.otalora@mi.unc.edu.ar>
2023-10-30 20:37:11 -0300

    Revert "ADC y UART (Rompieron a DMA:()"
    This reverts commit e30a7473ca33a15045601cc6477424bf9f8bb2fe.

--- Debug/src/main.d
+++ Debug/src/main.d
@@ -4,26 +4,22 @@ src/main.o src/main.d: ../src/main.c \
  C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/core_cmInstr.h \
  C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/core_cmFunc.h \
  C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h \
- C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_pinsel.h \
- C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/lpc17xx.h \
- C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h \
- C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpio.h \
- C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_dac.h \
  C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_adc.h \
+ C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h \
+ C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_dac.h \
  C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpdma.h \
  C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_timer.h \
- C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_clkpwr.h
+ C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_clkpwr.h \
+ C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/lpc17xx.h
 C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/LPC17xx.h:
 C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/core_cm3.h:
 C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/core_cmInstr.h:
 C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/core_cmFunc.h:
 C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h:
-C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_pinsel.h:
-C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/lpc17xx.h:
-C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h:
-C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpio.h:
-C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_dac.h:
 C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_adc.h:
+C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h:
+C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_dac.h:
 C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpdma.h:
 C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_timer.h:
 C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_clkpwr.h:
+C:\WorkspaceTPFINAL\CMSISv2p00_LPC17xx\inc/lpc17xx.h:
